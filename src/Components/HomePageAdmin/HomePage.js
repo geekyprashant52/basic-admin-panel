fe73f85289e7088da5e609d1c5ee99516144e517
geekyprashant52 <61961514+geekyprashant52@users.noreply.github.com>
2021-07-21 13:18:19 +0530

    updated api

--- src/Components/HomePageAdmin/HomePage.js
+++ src/Components/HomePageAdmin/HomePage.js
@@ -5,8 +5,9 @@ import classes from "./HomePageAdmin.module.css";
 import useFetchApi from "../Custom Hooks/useFetchApi";
 
 export default function HomePage() {
-  const url =
-    "http://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D";
+  // const url =
+  //   "http://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D";
+  const url = "https://60f7cc909cdca00017454ffd.mockapi.io/apidata/users";
   const { apiResults, isLoading } = useFetchApi(url);
   const [position, setPosition] = useState(-1);
   const [isClicked, setIsClicked] = useState(false);
